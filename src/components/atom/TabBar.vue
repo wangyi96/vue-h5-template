06c54181f37527f1248cd7660b71cc80bd892e98
yi wang <wangyi_mail08@163.com>
2021-01-14 14:28:09 +0800

    添加底部tabbar模块

--- src/components/atom/TabBar.vue
+++ src/components/atom/TabBar.vue
@@ -4,41 +4,12 @@
       <van-tabbar-item icon-prefix='iconfont' icon="home" :to="{name:'Home'}">
         <span>首页</span>
       </van-tabbar-item>
-      <van-tabbar-item icon-prefix='iconfont' icon="client" :to="{name:'User'}">
-        <span>客户</span>
-      </van-tabbar-item>
-      <!-- <van-tabbar-item @click="showAdd = true">
-        <img class="tui" src="../../assets/images/fastAdd/add.png" alt="">
-      </van-tabbar-item> -->
-      <van-tabbar-item icon-prefix='iconfont' icon="report" :to="{name:'Report'}">
-        <span>报表</span>
-      </van-tabbar-item>
       <van-tabbar-item icon-prefix='iconfont' icon="mine" :to="{name:'About'}">
         <span>我的</span>
       </van-tabbar-item>
     </van-tabbar>
 
     <div class="blank"></div>
-
-    <van-popup v-model="showAdd" round>
-      <div class="fastAdd">
-        <div class="head">新建项目</div>
-        <div class="content">
-          <div class="flex-column-center item" @click="goPath('createUser')">
-            <img src="../../assets/images/fastAdd/adduser.png" alt="" >
-            <span>新增客户</span>
-          </div>
-          <div class="flex-column-center item" @click="goPath('createContract')">
-            <img src="../../assets/images/fastAdd/addconstract.png" alt="" >
-            <span>新建合同</span>
-          </div>
-          <div class="flex-column-center item" @click="goPath('addPayBack')">
-            <img src="../../assets/images/fastAdd/addfollow.png" alt="" >
-            <span>新增回款</span>
-          </div>
-        </div>
-      </div>
-    </van-popup>
   </div>
 </template>
 <script>
@@ -60,7 +31,6 @@ export default {
   },
   data() {
     return {
-      showAdd:false,
       active: this.defaultActive
     }
   },
@@ -69,7 +39,6 @@ export default {
   methods: {
     goPath(name){
       if(name){
-        this.showAdd = false;
         this.$router.push({
           name
         })
@@ -97,7 +66,7 @@ export default {
   .head{
     width: 115px;
     height: 25px;
-    background: url('../../assets/images/fastAdd/head_bg.png') no-repeat  0/100%;
+    // background: url('../../assets/images/fastAdd/head_bg.png') no-repeat  0/100%;
     margin: 0 auto;
     font-size: 16px;
     color: #fff;
@@ -131,7 +100,7 @@ export default {
 .tabbar-wrap{
   .van-tabbar{
     z-index: 99;
-    background: rgba(255,255,255,.0);
+    // background: rgba(255,255,255,.0);
     box-shadow: 0px 0px 9px 0px
 		rgba(205, 205, 205, 0.55);
     /deep/ .van-tabbar-item:not(:nth-child(3)){
